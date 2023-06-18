09fe9da2219b89ca8a267c0c746ee55be3b5fd61
Matif408 <atifanwar408@gmail.com>
2023-06-18 16:18:09 +0500

    Create fibbinoci_series.cpp

---
+++ fibbinoci_series/fibbinoci_series.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int user_input;
+	cout<<"Enter the number for fibbinoci";
+	cin>>user_input;
+	int start = 0;
+	int next = 1;
+	int fibbinoci;
+	for(int i = 0; i < user_input; i++){
+	
+	
+		fibbinoci = start + next;
+		start = next;
+		next = fibbinoci;
+	    cout<<fibbinoci;
+	}
+	
+	
+	return 0;
+}
